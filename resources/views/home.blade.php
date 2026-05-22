52163b76ad150ab1fab009b375b2dbac3ce68d0a
PolloFelizdgo <soporte@pollofelizdgo.com>
2026-05-22 10:13:32 -0600

    tercer commit proyecto Pollo Feliz

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,22 +3,27 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <title>Pollo Feliz | Landing Page</title>
+
+    <link rel="icon" type="image/png" href="{{ asset('logo.ico') }}">
+
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
+
 <body class="bg-[#fffaf5] text-gray-800">
-@include('partials.navbar')
-@include('partials.hero')
-@include('partials.surcusales')
-@include('partials.menu')
-@include('partials.promociones')
-@include('partials.acerca')
-@include('partials.contacto')
-@include('partials.footer')
+
+    @include('partials.navbar')
+    @include('partials.hero')
+    @include('partials.surcusales')
+    @include('partials.menu')
+    @include('partials.promociones')
+    @include('partials.acerca')
+    @include('partials.contacto')
+    @include('partials.footer')
+
 </body>
 </html>
-
-
 
 
 
